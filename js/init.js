a64060df76e87a3afe79244445fdf55db79c583c
Fabian <fega.hg@gmail.com>
2016-03-21 12:58:03 -0500

    minor improvements

--- js/init.js
+++ js/init.js
@@ -1,8 +1,18 @@
-(function($){
-  $(function(){
+(function ($) {
+	$(function () {
+		$('.button-collapse').sideNav();
+		$('.parallax').parallax();
+		$('.slider').slider({
+			full_width: true,
+			height: 800
+		});
+		$(".dropdown-button").dropdown();
+		$('select').material_select();
+		$('nav').pushpin({
+			top: 0,
+			offset: 0,
+		});
+		$('input#input_text, textarea#textarea1').characterCounter();
 
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
-
-  }); // end of document ready
+	}); // end of document ready
 })(jQuery); // end of jQuery name space
